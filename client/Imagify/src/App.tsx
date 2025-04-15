f936f7b410bd0ac1e7f957ebd26fbac6fcc3aae0
glen654 <glenalloy@gmail.com>
2025-04-15 11:52:47 +0530

    implemented routes for the project

--- client/Imagify/src/App.tsx
+++ client/Imagify/src/App.tsx
@@ -1,5 +1,18 @@
+import BuyCredit from "./pages/BuyCredit";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
